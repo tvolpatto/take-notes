d494cbc1cd7f90e38e7e5275b71a4f8a074d6f01
Thyago Volpatto <tvolpatto@doterra.com>
2020-02-07 12:11:34 -0700

    route  api/notes to get the notes; fixed css loading

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App
 var app = express();
 var PORT = 3000;
 
-// Middleware
+// App configuration
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//need it to load the css
+app.use('/public', express.static('public'));
 
+ 
 //------------------- Begin Routes -------------------\\
 app.get("/", function(req, res) {
-   res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-  
+
 app.get("/notes", function(req, res) {
-   res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+  fs.readFile('./db/db.json', "utf-8", (err, data) => {
+    if(err)  throw err;
+    
+    return res.json(JSON.parse(data));
+  });
 });
 //-------------------- End Routes --------------------\\
 
