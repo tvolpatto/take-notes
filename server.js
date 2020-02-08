b141d7d5961e48d7f3a57762b5b15a17171c431b
Thyago Volpatto <tvolpatto@gmail.com>
2020-02-07 20:27:32 -0700

    get port dynamically

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 
 // Sets up the Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // App configuration
 app.use(express.urlencoded({ extended: true }));
